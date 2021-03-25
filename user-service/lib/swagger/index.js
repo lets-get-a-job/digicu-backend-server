dc76dcb577ead950f62f54f6de8f30ce5e0e759f
JeHwanYoo <yoojehwan1875@gmail.com>
2021-03-26 01:57:26 +0900

    update: API document 업데이트

--- user-service/lib/swagger/index.js
+++ user-service/lib/swagger/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const YAML = require('yamljs');
 const usersAPIDocument = YAML.load(
-  path.resolve(__dirname, 'docs', 'users.yml'),
+  path.resolve(__dirname, 'docs', 'User Service.yml'),
 );
 
 module.exports = {
--- user-service/routes/users.js
+++ user-service/routes/users.js
@@ -4,62 +4,6 @@ const router = express.Router();
 const { registraton, search, authentication } = require('../lib/query/users');
 const { errorHandling } = require('../lib/routing');
 
-router.get('/:email/auth', (req, res) => {
-  if (req.query.type === 'reg' && req.query.token) {
-    res.render('email-auth', {
-      email: req.params.email,
-      token: req.query.token,
-    });
-  } else {
-    res.sendStatus(400);
-  }
-});
-
-router.post('/:email/auth', async (req, res) => {
-  if (req.query.type === 'pwd') {
-    const token = await authentication.createChangePasswordToken(
-      req.params.email,
-      req.body.old_encpwd,
-    );
-    if (token) {
-      res.json({ token });
-    } else {
-      res.sendStatus(400);
-    }
-  } else {
-    res.sendStatus(400);
-  }
-});
-
-router.put('/:email/auth', async (req, res) => {
-  try {
-    if (req.query.type === 'reg') {
-      const isAuthOk = await authentication.confirmEmailAuth(
-        req.params.email,
-        req.body.token,
-      );
-      if (isAuthOk) {
-        res.sendStatus(200);
-      } else {
-        res.sendStatus(400);
-      }
-    } else if (req.query.type === 'pwd') {
-      const isPasswordChanged = await authentication.changePassword(
-        req.params.email,
-        req.body.new_encpwd,
-        req.body.token,
-      );
-      if (isPasswordChanged) {
-        res.sendStatus(200);
-      } else {
-        res.sendStatus(400);
-      }
-    }
-  } catch (error) {
-    errorHandling.sendError(res, 400, '인증 실패', error);
-  }
-});
-
 // router.get('/change_password_test', async (req, res) => {
 //   res.render('change-password-test', {
 //     email: 'formail0001@naver.com',
