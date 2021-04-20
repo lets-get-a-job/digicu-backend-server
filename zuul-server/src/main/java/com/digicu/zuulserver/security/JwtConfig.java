99a2591ea6a7d290d9b9acafe79e2123a77fef34
whoishu <dydgndlaek97@gmail.com>
2021-04-20 21:48:32 +0900

    fix: typo error

--- zuul-server/src/main/java/com/digicu/zuulserver/security/JwtConfig.java
+++ zuul-server/src/main/java/com/digicu/zuulserver/security/JwtConfig.java
@@ -6,7 +6,7 @@ public class JwtConfig {
     @Value("${security.jwt.header:Authorization}")
     private String header;
 
-    @Value("${security.jwt.prefix:Bearer }")
+    @Value("${security.jwt.prefix:Bearer}")
     private String prefix;
 
     @Value("${security.jwt.secret:86ae5efb008c10740e15}")
