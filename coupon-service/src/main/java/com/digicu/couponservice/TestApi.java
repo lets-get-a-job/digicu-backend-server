8ab97f6df0947fcd73f5b65aeb15a82d09b39100
whoishu <dydgndlaek97@gmail.com>
2021-05-22 01:54:13 +0900

    change: testapi 컨트롤러 이름 변경

--- coupon-service/src/main/java/com/digicu/couponservice/TestApi.java
+++ coupon-service/src/main/java/com/digicu/couponservice/TestApi.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/coupon/test")
-public class Controller {
+public class TestApi {
     private final FCMService fcmService;
 
     @PostMapping("/fcm/{phone}")
