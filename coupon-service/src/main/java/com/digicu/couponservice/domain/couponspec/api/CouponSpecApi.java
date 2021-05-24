8145e903630c347966877167c36ed34008c52979
whoishu <dydgndlaek97@gmail.com>
2021-05-24 13:56:27 +0900

    new: 이미지 업로드 기능

--- coupon-service/src/main/java/com/digicu/couponservice/domain/couponspec/api/CouponSpecApi.java
+++ coupon-service/src/main/java/com/digicu/couponservice/domain/couponspec/api/CouponSpecApi.java
@@ -6,12 +6,17 @@ import com.digicu.couponservice.domain.couponspec.dto.CouponSpecCreateRequest;
 import com.digicu.couponservice.domain.couponspec.dto.CouponSpecUpdateRequest;
 import com.digicu.couponservice.domain.couponspec.service.CouponSpecService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FileUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -19,7 +24,7 @@ import java.util.List;
 @RequestMapping("/coupon/coupon_spec")
 public class CouponSpecApi {
     final private CouponSpecService couponSpecService;
-    final private CouponSpecFindDao couponSpecFindDao; // Remove after
+    final private CouponSpecFindDao couponSpecFindDao;
 
     @PostMapping
     public ResponseEntity<CouponSpec> createCouponSpec(
@@ -63,6 +68,20 @@ public class CouponSpecApi {
     ){
         List<CouponSpec> specs = couponSpecFindDao.findAllByEmail(email);
         return new ResponseEntity<List<CouponSpec>>(specs, HttpStatus.OK);
+    }
+
+    @PostMapping("/img")
+    public ResponseEntity<String> uploadImg(@RequestParam("file") MultipartFile multipartFile){
+        File targetFile = new File("coupon-service/upload/" + multipartFile.getOriginalFilename());
+        try {
+            InputStream fileStream = multipartFile.getInputStream();
+            FileUtils.copyInputStreamToFile(fileStream, targetFile);
+            return new ResponseEntity<String>(targetFile.getAbsolutePath(), HttpStatus.OK);
+        } catch (IOException e) {
+            FileUtils.deleteQuietly(targetFile);
+            e.printStackTrace();
+            return new ResponseEntity<String>("fail", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
 
